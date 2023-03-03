9e781237886b1dc193a09da5dd8118b656e0a3d0
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2023-03-03 00:03:10 -0300

    added new correct unit test

--- app/src/test/java/com/devlemos/samplewithgithubaction/ExampleUnitTest.kt
+++ app/src/test/java/com/devlemos/samplewithgithubaction/ExampleUnitTest.kt
@@ -14,4 +14,11 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun new_addition_isCorrect() {
+        assertEquals(6, 3 + 3)
+    }
+
+
 }
